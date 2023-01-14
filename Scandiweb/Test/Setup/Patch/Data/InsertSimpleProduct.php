f52a085ef736299cb5e8d9dae3dccb92adc17c75
abdelrhmanyosry <abdelrhman.yosry@scandiweb.com>
2023-01-14 16:39:59 +0200

    Remove Unessary files and assign category to product

---
+++ Scandiweb/Test/Setup/Patch/Data/InsertSimpleProduct.php
@@ -0,0 +1,156 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Scandiweb\Test\Setup\Patch\Data;
+
+
+use Magento\Catalog\Api\CategoryLinkManagementInterface;
+use Magento\Catalog\Api\Data\ProductInterfaceFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Framework\App\State;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+
+
+class InsertSimpleProduct implements DataPatchInterface
+{
+
+
+    protected ProductInterfaceFactory $productInterfaceFactory;
+
+    protected ProductRepositoryInterface $productRepository;
+
+    protected State $appState;
+
+    protected EavSetup $eavSetup;
+
+    protected StoreManagerInterface $storeManager;
+
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    protected CategoryLinkManagementInterface $categoryLink;
+
+    protected array $sourceItems = [];
+
+    protected CategoryCollectionFactory $categoryCollectionFactory;
+
+
+    public function __construct(
+        ProductInterfaceFactory $productInterfaceFactory,
+        ProductRepositoryInterface $productRepository,
+        State $appState,
+        StoreManagerInterface $storeManager,
+        EavSetup $eavSetup,
+        SourceItemInterfaceFactory $sourceItemFactory,
+        SourceItemsSaveInterface $sourceItemsSaveInterface,
+        CategoryLinkManagementInterface $categoryLink,
+        CategoryCollectionFactory $categoryCollectionFactory
+
+    ) {
+        $this->appState = $appState;
+        $this->productInterfaceFactory = $productInterfaceFactory;
+        $this->productRepository = $productRepository;
+        $this->setup = $setup;
+        $this->eavSetup = $eavSetup;
+        $this->storeManager = $storeManager;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
+        $this->categoryLink = $categoryLink;
+        $this->categoryCollectionFactory = $categoryCollectionFactory;
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()  :array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply() :void
+    {
+        $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
+    }
+
+     /**
+     * {@inheritdoc}
+     */
+    public function getAliases() :array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public function execute() :void
+    {
+
+        // create the product
+        $product = $this->productInterfaceFactory->create();
+
+        // check if the product already exists
+        if ($product->getIdBySku('new-product-1-2023')) {
+            return;
+        }
+
+        // set default attributes...
+        // get the attribute set id from EavSetup object
+        $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
+
+        // set attributes
+        $product->setTypeId(Type::TYPE_SIMPLE)
+            ->setAttributeSetId($attributeSetId)
+            ->setName('new-Product-2023')
+            ->setSku('new-Product-1-2023')
+            ->setUrlKey('newproduct')
+            ->setPrice(9.99)
+            ->setVisibility(Visibility::VISIBILITY_BOTH)
+            ->setStatus(Status::STATUS_ENABLED);
+        // save the product to the repository
+        $product = $this->productRepository->save($product);
+
+        // set source item...\
+
+        // get the website id from a StoreManagerInterface instance
+        $websiteIDs = [$this->storeManager->getStore()->getWebsiteId()];
+
+        $product->setWebsiteIds($websiteIDs);
+
+        $product->setStockData(['use_config_manage_stock' => 1, 'is_qty_decimal' => 0, 'is_in_stock' => 1]); 
+
+        // create a source item
+		$sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+            // set the quantity of items in stock
+        $sourceItem->setQuantity(100);
+            // add the product's SKU that will be linked to this source item
+        $sourceItem->setSku($product->getSku());
+            // set the stock status
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+        $this->sourceItems[] = $sourceItem;
+    
+            // save the source item
+        $this->sourceItemsSaveInterface->execute($this->sourceItems);
+
+        $this->categoryLink->assignProductToCategories($product->getSku(), [3]);
+
+    }
+}
